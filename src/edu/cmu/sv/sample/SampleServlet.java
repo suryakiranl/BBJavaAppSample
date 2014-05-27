47bddc4c056a393116f281d064fb24fb2ddc7fa4
Surya Kiran <suryakiranl@gmail.com>
2014-05-26 23:15:27 -0700

    Sample servlet works

--- src/edu/cmu/sv/sample/SampleServlet.java
+++ src/edu/cmu/sv/sample/SampleServlet.java
@@ -1,6 +1,8 @@
 package edu.cmu.sv.sample;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -11,26 +13,44 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class SampleServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public SampleServlet() {
-        super();
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+	public SampleServlet() {
+		super();
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+
+		String responseStr = "";
+
+		String userName = request.getParameter("userName");
+		if (userName == null || userName.trim().equals("")) {
+			responseStr = "ERROR: User name provided was empty. "
+					+ "Please provide a valid name!!";
+		} else {
+			responseStr = "Hello " + userName + " !!";
+		}
 		
+		responseStr += "<br/><a href='index.html'>Go Back</a>";
+
+		PrintWriter out = response.getWriter();
+		out.println(responseStr);
 	}
 
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
 }
